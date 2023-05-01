5a8d4cf349e1aecdf02aa109383fff89c9ef76e9
builtbyjosh <joshuacollins912@gmail.com>
2023-05-01 14:04:01 -0500

    instal router and create blank pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
+import Login from "./pages/login/Login";
+import Create from "./pages/create/Create";
+import Signup from "./pages/signup/Signup";
+import Project from "./pages/project/Project";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/create" component={Create} />
+            <Route path="/projects/:id" component={Project} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
