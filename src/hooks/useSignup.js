364615ce5f024562c7e063d7524b37583fadc72d
builtbyjosh <joshuacollins912@gmail.com>
2023-05-01 17:08:51 -0500

    complete fb user creation

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
-import { auth } from "../firebase/config";
+import { auth, storage } from "../firebase/config";
 import { useAuthContext } from "./useAuthContext";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { updateProfile, createUserWithEmailAndPassword } from "firebase/auth";
 
 export const useSignup = () => {
   const [isCancelled, setIsCancelled] = useState(false);
@@ -8,20 +10,34 @@ export const useSignup = () => {
   const [isPending, setIsPending] = useState(false);
   const { dispatch } = useAuthContext();
 
-  const signup = async (email, password, displayName) => {
+  const signup = async (email, password, displayName, thumbnail) => {
     setError(null);
     setIsPending(true);
 
     try {
       // signup
-      const res = await auth.createUserWithEmailAndPassword(email, password);
+      // const res = await auth.createUserWithEmailAndPassword(email, password);
+      const res = await createUserWithEmailAndPassword(auth, email, password);
 
       if (!res) {
         throw new Error("Could not complete signup");
       }
 
-      // add display name to user
-      await res.user.updateProfile({ displayName });
+      // // upload user thumbnail
+      // const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`
+      // const img = await storage.ref(uploadPath).put(thumbnail)
+      // const imgUrl = await img.ref.getDownloadURL()
+      const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`;
+      const imgRef = ref(storage, uploadPath);
+      await uploadBytes(imgRef, thumbnail);
+      const imgUrl = await getDownloadURL(imgRef);
+
+      // Add display name to user
+      // await res.user.updateProfile({ displayName, photoURL: imgUrl });
+      await updateProfile(res.user, {
+        displayName,
+        photoURL: imgUrl,
+      });
 
       // dispatch login action
       dispatch({ type: "LOGIN", payload: res.user });
