d3cea78cd6de8b24333b361520e73b38ea21404b
builtbyjosh <joshuacollins912@gmail.com>
2023-05-03 10:42:36 -0500

    add avatar to sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Login from "./pages/login/Login";
 import Create from "./pages/create/Create";
@@ -7,23 +7,29 @@ import Signup from "./pages/signup/Signup";
 import Project from "./pages/project/Project";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const {user, authIsReady} = useAuthContext()
+
   return (
     <div className="App">
-      <Router>
-        <Sidebar />
+      {authIsReady && (
+
+        <Router>
+        {user && <Sidebar />}
         <div className="container">
           <Navbar />
           <Routes>
-            <Route exact path="/" element={<Dashboard />} />
-            <Route path="/create" element={<Create />} />
-            <Route path="/projects/:id" element={<Project />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route exact path="/" element={user ? <Dashboard /> : <Navigate to="login" />} />
+            <Route path="/create" element={user ? <Create /> : <Navigate to="/login" />} />
+            <Route path="/projects/:id" element={user ? <Project /> : <Navigate to="/login" />} />
+            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+            <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
           </Routes>
         </div>
       </Router>
+        )}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,12 @@ import "./Navbar.css";
 import Temple from "../assets/temple.svg";
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
+
   return (
     <nav className="navbar">
       <ul>
@@ -13,24 +16,30 @@ const Navbar = () => {
           <img src={Temple} alt="logo" />
           <Link to="/">Tuesday</Link>
         </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          {!isPending && (
-            <button className="btn" onClick={logout}>
-              Logout
-            </button>
-          )}
-          {isPending && (
-            <button className="btn" disabled>
-              Logging Out..
-            </button>
-          )}
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            {!isPending && (
+              <button className="btn" onClick={logout}>
+                Logout
+              </button>
+            )}
+            {isPending && (
+              <button className="btn" disabled>
+                Logging Out..
+              </button>
+            )}
+          </li>
+        )}
       </ul>
     </nav>
   );
