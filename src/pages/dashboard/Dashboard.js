65966fd3a06d54e9e539da2a8cfbfc48b66b8826
builtbyjosh <joshuacollins912@gmail.com>
2023-05-08 13:28:29 -0500

    Setup dashboard

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./Dashboard.css";
+import { useCollection } from "../../hooks/useCollection";
+import ProjectList from "../../components/ProjectList";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  const { documents, error } = useCollection("projects");
+  console.log("PROJECTS: ", documents);
+  return (
+    <div>
+      <h2 className="page-title">Dashboard</h2>
+      {error && <p className="error">{error}</p>}
+      {documents && <ProjectList projects={documents} />}
+    </div>
+  );
 };
 
 export default Dashboard;
